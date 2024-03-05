a2a89dda56752c48db70446e5e820c590abb5325
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-05 16:47:24 +0900

    change(a, b)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+// [문제] main을 수정하지 않고 의도대로 실행되게
+// change함수를 선언하고 정의하라
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = Dog { data: 1 };
+    let mut b = Dog { data: 2 };
+
+    println!("{:?}, {:?}", a, b);
+
+    change(&mut a, &mut b);
+
+    println!("{:?}, {:?}", a, b);
+}
+
+
+#[derive(Debug)]
+struct Dog {
+    data: i32
+}
+
+impl Clone for Dog {
+    fn clone(&self) -> Dog {
+        Dog {
+            data: self.data
+        }
+    }
+}
+
+fn change<T: Clone>(a: &mut T, b: &mut T) {
+    let temp = a.clone();
+    *a = b.clone();
+    *b = temp;
 }
