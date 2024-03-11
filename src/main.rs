a4651fd2e9d42890889e68eaf75543a1b8765cc2
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-11 17:56:59 +0900

    find max

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 // [문제] 가장 큰 수를 찾아 화면에 출력한다.
 
-
 use rand::Rng;
 
 fn main() {
-    let mut a: [i32;1000] = [0;1000];
+    let mut a = [0;1000];
+
     for num in a.iter_mut() {
         *num = rand::thread_rng().gen_range(1000..9999);
         print!("{:8}", num);
     }
-
 
     println!("\n{}", a.iter().max().unwrap());
 }
