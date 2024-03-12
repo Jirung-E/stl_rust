804c8120a3d36ef575cc204a64a61b29d3ed7257
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-12 16:09:09 +0900

    iter_mut().enumerate()

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,11 @@
-use rand::Rng;
-use std::fs::File;
-use std::io::{Read, Write};
-
 fn main() {
-    find_max();
-}
+    let mut a = [0;100];
 
-fn make_file() {
-    let mut file = match File::create("random_numbers.txt") {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("Problem creating the file: {:?}", error)
-        },
-    };
-
-    let mut a = [0;1000];
-
-    for num in a.iter_mut() {
-        *num = rand::thread_rng().gen_range(0..=99_999);
-        file.write(format!("{}\n", num).as_bytes()).unwrap();
+    for (i, num) in a.iter_mut().enumerate() {
+        *num += i;
     }
-}
 
-fn find_max() {
-    let mut file = match File::open("random_numbers.txt") {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("Problem opening the file: {:?}", error)
-        },
-    };
-
-    let mut buf = String::new();
-    file.read_to_string(&mut buf).unwrap();
-
-    println!("{}",
-        buf.lines()
-            .map(|x| { x.parse::<i32>().unwrap() })
-            .collect::<Vec<i32>>().iter()
-            .max().unwrap()
-    );
+    for num in a.iter() {
+        print!("{:8}", num);
+    }
 }
