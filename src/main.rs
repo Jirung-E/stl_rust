b17865b2584b91667db828497121ba958bd5ef68
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-25 18:47:41 +0900

    range collect, sum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-fn main() {
-    let mut a = (1..=100).collect::<Vec<_>>();
+use std::io::Write;
 
-    a.iter().for_each(|x| println!("{}", x));
-    println!("sum: {}", a.iter().sum::<i32>());
+fn main() {
+    loop {
+        print!("숫자를 입력하라 - ");
+        std::io::stdout().flush().unwrap();
+        let num = input().trim().parse::<u32>().unwrap();
+        if num == 0 {
+            break;
+        }
+
+        let v: Vec<u32> = (1..=num).collect();
+
+        // println!("{:?}", v);
+
+        // u128로 sum
+        let sum: u128 = v.iter().map(|x| *x as u128).sum();
+        // sum::<u128>이 안된다...
+        println!("1부터 {}까지의 합은 {}", num, sum);
+    }
+}
+
+fn input() -> String {
+    let mut buf = String::new();
+    std::io::stdin()
+        .read_line(&mut buf)
+        .unwrap();
+    buf
 }
