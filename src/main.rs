93155dcfd0917c17f6189b307994e22e4a9fc330
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-03-25 20:21:03 +0900

    게으른 반복자

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,13 @@ fn main() {
             break;
         }
 
-        let v: Vec<u32> = (1..=num).collect();
-
-        // println!("{:?}", v);
-
         // u128로 sum
-        let sum: u128 = v.iter().map(|x| *x as u128).sum();
+        // 게으른 반복자 덕분에 u128 * num 만큼의 메모리를 잡아먹진 않는다.
+        let sum: u128 = (1..=num).map(|x| x as u128).sum();
         // sum::<u128>이 안된다...
         println!("1부터 {}까지의 합은 {}", num, sum);
+
+        input();
     }
 }
 
