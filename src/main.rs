bed68dee9842bf2bffe39a9cac172f3e85081e37
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-04-02 17:12:18 +0900

    2차원 랜덤 배열 만들기

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
-use std::io::Write;
+use rand::Rng;
 
 fn main() {
-    loop {
-        print!("숫자를 입력하라 - ");
-        std::io::stdout().flush().unwrap();
-        let num = input().trim().parse::<u32>().unwrap();
-        if num == 0 {
-            break;
+    let d1 = (0..100)
+        .map(|_| rand::thread_rng().gen_range(0..100))
+        .collect::<Vec<i32>>();
+    println!("{:?}", d1);
+
+    let d2 = (0..10)
+        .map(|_| {
+            (0..10)
+                .map(|_| rand::thread_rng().gen_range(0..100))
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>();
+    for i in d2.iter() {
+        for k in i.iter() {
+            print!("{:4}", k);
         }
-
-        // u128로 sum
-        // 게으른 반복자 덕분에 u128 * num 만큼의 메모리를 잡아먹진 않는다.
-        let sum: u128 = (1..=num).map(|x| x as u128).sum();
-        // sum::<u128>이 안된다...
-        println!("1부터 {}까지의 합은 {}", num, sum);
-
-        input();
+        println!();
     }
-}
-
-fn input() -> String {
-    let mut buf = String::new();
-    std::io::stdin()
-        .read_line(&mut buf)
-        .unwrap();
-    buf
 }
