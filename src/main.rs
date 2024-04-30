fae0f78d9ae4afcaa6dfbc1a9be691bbc9ea0671
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-04-30 17:00:28 +0900

    vector concat

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use std::fs;
-
 fn main() {
-    // [문제] "stl.cpp"에 있는 알파벳 소문자의 출현 횟수를 다음과 같이 출력하라
-    // a - 20
-    // b - 3
-    // c - 1
-    // ...
-    // z - 2
+    let a = vec![1, 2, 3, 4];
+    let b = vec![5, 6, 7, 8];
 
-    let file = fs::read_to_string("src/main.rs")
-        .expect("파일을 읽을 수 없습니다.");
-
-    let mut counts = vec![0; 'z' as usize - 'a' as usize + 1];
-
-    file.chars()
-        .filter(|c| c.is_lowercase())
-        .for_each(|c| counts[c as usize - 'a' as usize] += 1);
-
-    for (i, count) in counts.iter().enumerate() {
-        println!("{} - {}", (i as u8 + 'a' as u8) as char, count);
-    }
+    let v = [a, b].concat();
+    println!("{:?}", v);
 }
