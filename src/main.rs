c093a3683af6e22de4afb84fbed355772d9f9865
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-06-03 17:57:57 +0900

    all == c++::all_of

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let v: [i32; 5] = [1, 3, 5, 7, 9];
+    let v: [i32; 0] = [];
 
     if v.iter().all(|x| { x & 1 == 1 }) {
-        println!("모두 홀수");
+        println!("홀수가 아닌것은 한개도 없다.");
     }
     else {
-        println!("모두 홀수인 것은 아니다.");
+        println!("홀수가 아닌게 있다.");
     }
 }
