8c2c1665c14542bd537c770ae94b280bbb66334a
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-06-04 16:50:42 +0900

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-fn main() {
-    let mut v = vec![3, 1, 4, 1, 5, 9, 2, 6];
-    let (even, odd): (Vec<_>, Vec<_>) = v
-        .into_iter()
-        .partition(|n| n % 2 == 0);
 
-    println!("{:?}", even);
-    println!("{:?}", odd);
+fn main() {
+    println!("{}", add(1, 2));
+    println!("{}", add(1.1, 2.2));
+    // println!("{}", add("Hello", " World"));
 }
+
+fn add<T>(a: T, b: T) -> T
+where
+    T: std::ops::Add<Output = T>,
+{
+    a + b
+}
+
