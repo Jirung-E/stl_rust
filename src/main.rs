10ef0a5c9305f87e2ac8939519a98dbe1b662b30
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-06-10 19:04:12 +0900

    sort by key

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
+use rand::Rng;
 use rand::seq::SliceRandom;
 
 fn main() {
-    let mut v: Vec<i32> = (0..100).collect();
+    let mut v: Vec<Dog> = Vec::from_iter((0..100).map(|_| Dog::new()));
 
-    for num in v.iter() {
-        print!("{:4}", num);
+    for dog in &v {
+        println!("{} --- {}", dog.n, dog.c);
     }
     println!();
     println!();
 
-    v.shuffle(&mut rand::thread_rng());
+    v.sort_by_key(|dog| dog.n);
 
-    for num in v.iter() {
-        print!("{:4}", num);
+    for dog in &v {
+        println!("{} --- {}", dog.n, dog.c);
     }
     println!();
     println!();
+}
 
-    v.sort();
 
-    for num in v.iter() {
-        print!("{:4}", num);
+struct Dog {
+    n: i32,
+    c: char,
+}
+
+impl Dog {
+    fn new() -> Dog {
+        Dog {
+            n: rand::thread_rng().gen_range(1..10),
+            c: rand::thread_rng().gen_range('a'..='z'),
+        }
     }
 }
