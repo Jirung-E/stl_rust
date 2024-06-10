74ace1d294de22904f0191bb2e5a1794e5b33190
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-06-10 21:22:22 +0900

    iter::successors: = cpp::ranges::iota(0) 근데 C++의 iota보다 느리다?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::iter;
+use std::time::Instant;
 
 fn main() {
-    iter::successors(Some(0), |i| Some(i + 1))
-        .take(10)
-        .for_each(|i| {
-            println!("{}", i);
-        });
+    let start = Instant::now();
+    let v: Vec<_> = iter::successors(Some(1), |i| Some(i + 1))
+        .skip(10000)      // cpp::views::drop
+        .take(1_000_000)       // cpp::views::take
+        .collect();      // cpp::ranges::to<vector>
+    println!("Time: {:?} micro seconds", start.elapsed().as_micros());
+
+    // for num in v {
+    //     print!("{} ", num);
+    // }
 }
